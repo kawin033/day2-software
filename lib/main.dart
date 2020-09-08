ab05d4d92d15148bf8853362651cd86ae4440f51
basirah1266 <basirah.c@ku.th>
2020-09-08 07:19:45 +0700

    basic widget stack

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,86 @@ class MyApp extends StatelessWidget {
 
           color: Colors.blue,
           child: Testwidget(),
-          height: 500.0,
-          width: 500.0,
+          height: 1000.0,
+          width: 1000.0,
         )
       ),
     );
   }
 }
+//class Testwidget1 extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Text(
+//      'Hello',
+ //     style: TextStyle(
+ //       fontSize: 50,
+  //      color: Color(0xff00bfa5),
+  //      fontWeight: FontWeight.w800,
+ //     ),
+ //   );
+  
+ // }
+//} 
+//class Testwidget extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Icon(
+//      Icons.accessibility,
+//      size: 100,
+//      color: Colors.greenAccent,
+/*       
+//    );
+//  }
+//}
 class Testwidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Hello',
-      style: TextStyle(
-        fontSize: 50,
-        color: Color(0xff00bfa5),
-        fontWeight: FontWeight.w800,
-      ),
-      );
-  
+    return Column(
+      children: [
+        Text("data"),
+        Image.network(
+          'https://stickershop.line-scdn.net/stickershop/v1/product/582/LINEStorePC/main.png;compress=true',
+        ),
+      ],
+    );
   }
-} 
+} */
+class Testwidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+     /*  mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center, */
+      alignment: Alignment.center,
+      children: [
+        Container(
+          height: 300.0,
+          width: 300.0,
+          color: Colors.teal,
+        ),
+        Container(
+          height: 200.0,
+          width: 200.0,
+          color: Colors.deepPurple
+          ),
+          Container(
+          height: 100.0,
+          width: 100.0,
+          color: Colors.brown,
+        ),
+        Container(
+          height: 50.0,
+          width: 50.0,
+          color: Colors.yellow,
+        ),
+        Container(
+          height: 25.0,
+          width: 25.0,
+          color: Colors.indigo,
+        ),
+      ],
+    );
+  }
+}
+
